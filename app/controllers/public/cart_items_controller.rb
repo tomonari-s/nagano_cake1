8d422e6dbf58df4d90374cf6752deb1131c4d0a9
tomonari-s <108882451+tomonari-s@users.noreply.github.com>
2022-09-28 11:20:36 +0000

    220928_2020

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,26 +1,34 @@
 class Public::CartItemsController < ApplicationController
   def index
-    # @cart_items = Cart_item.all
+    @cart_items = CartItem.all
+    @total = 0
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to public_cart_items_path(@cart_item.id)
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to public_cart_items_path
   end
 
   def destroy_all
   end
 
   def create
-    binding.pry
-    @cart_item = Cart_item.new(genre_params)
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id 
     @cart_item.save
-    redirect_to '/admin/genres'
+    redirect_to public_cart_items_path
   end
   
   private
   def cart_item_params
-      params.require(:cart_item).permit(:item_id, :amount)
+      params.permit(:item_id, :amount)
+      # params.require(:cart_item).permit(:item_id, :amount)
   end
 end
